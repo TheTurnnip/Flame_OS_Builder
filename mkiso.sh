1a65e258dc9ed28ce6704b123808c64a2caf9536
Ryan S <ryansteffan.biz@gmail.com>
2022-04-05 22:18:45 -0500

    Added auto_login

--- mkiso.sh
+++ mkiso.sh
@@ -24,11 +24,13 @@ grab_pack(){
 
 copy_relang(){
     rm -rf ./flameos
+    rm -rf ./work
+    rm -rf ./temps
     cp -r /usr/share/archiso/configs/releng/ ./flameos
     package_def
 }
 
-#Clears the previous build directory and makes a new one
+#Clears the previous build directory and makes a new one.
 
 package_def(){
     rm -rf ./flameos/packages.x86_64;
@@ -38,15 +40,23 @@ package_def(){
     systemd_enabler;
 }
 
-#Clones and copies the packages that are included in the iso
+#Clones and copies the packages that are included in the iso.
 
 systemd_enabler(){
     ln -s /usr/lib/systemd/system/sddm.service ./flameos/airootfs/etc/systemd/system/multi-user.target.wants/
     ln -s /usr/lib/systemd/system/NetworkManager.service ./flameos/airootfs/etc/systemd/system/multi-user.target.wants/
+    auto_login
+}
+
+#Enables the systemd units that are used by the live iso.
+
+auto_login(){
+    mkdir ./flameos/airootfs/etc/sddm.conf.d/
+    echo -e "[Autologin] \nUser=root \nSession=plasma" > ./flameos/airootfs/etc/sddm.conf.d/autologin.conf
     build
 }
 
-#Enables the systemd units that are used by the live iso
+#Enables autologin for the live iso.
 
 build(){
     mkarchiso -v -w ./work -o ./iso/ ./flameos
